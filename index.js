c9c34b1b8526ce9528d32c80c7c82ad274ff86b8
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2024-08-30 20:15:44 +0200

    Create a constructor

--- index.js
+++ index.js
@@ -10,13 +10,31 @@ class BankBranch{
 
 // 3. In the `BankBranch` class:
 //    - Create a constructor that takes `branchInfo` as a parameter.
+
+constructor(branchInfo){
+
 //    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
+
+if(!bankBranchInstance){
+
 //    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
+
+this.branchInfo = branchInfo;
+     bankBranchInstance = this;
+}
+
 //    - Return the `bankBranchInstance` whether it's newly created or existing.
 
+return bankBranchInstance
 }
 
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
+
+getBranchInfo(){
+    return this.branchInfo;
+}
+
+}
 
 // 5. In the usage section:
 //    - Create instances of the `BankBranch` class, such as `branchA` and `branchB`, with different branch information.
